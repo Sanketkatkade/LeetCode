ba52cda7c07a9b9c73d258eea42fabb351af2b41
Mr.Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2023-04-07 14:18:29 +0530

    Time: 2541 ms (34.60%), Space: 6.2 MB (7.66%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        vector<int>v;
+        for(int i = 1; i <= num/2; i++){
+            if(num % i==0){
+                v.push_back(i);
+            }
+        }
+        
+        int sum = 0;
+        for(int i = 0; i < v.size(); i++){
+            sum += v[i];
+        }
+     
+        if(sum == num){
+            return true;
+        }
+        return false;
+    }
+};
