086865c977bb840579e5c3ac3d15591ac746c21b
Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2023-04-30 10:02:49 +0530

    Time: 7 ms (96.96%), Space: 10.2 MB (43.68%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int count=0;
+        for(int i=0;i<sentences.size();i++){
+            string word = sentences[i];
+            int word_count = 1;
+            for(int j = 0; j < word.length(); j++){
+                if(word[j]== ' ' && word[j+1]!= ' '){
+                    word_count++;
+                }
+            }
+           count = max (count , word_count);
+        }
+         return count ;;
+    }
+};
