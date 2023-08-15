1f5501184bb0c7ec820cb8b8b20aaadf0cc5d0ca
Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2023-08-15 21:19:22 +0530

    Time: 15 ms (31.71%), Space: 5.9 MB (91.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1){
-            return n;
-        }
-        int prev = 0;
-        int current = 1;
-        for(int i = 2; i <= n; i++){
-            int next = prev + current;
-            prev = current;
-            current = next;
-        }
-        return current;
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int ans = fib(n - 1) + fib(n - 2);
+        return ans;
     }
 };
