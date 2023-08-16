dd376f18dd7fca399d07ab4407f93320cc1b0cf4
Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2023-08-16 17:13:23 +0530

    Time: 7 ms (75.01%), Space: 13.6 MB (31.22%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(auto x : details){
+            int age = (x[11] - '0') * 10 + (x[12] - '0');
+            if(age > 60){
+                count++;
+            }
+        }
+        return count;
+    }
+};
