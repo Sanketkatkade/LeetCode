9fa0ba2432b6c2b77ab404f7d0d4cc5e3ddb78fe
Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2023-08-29 19:16:00 +0530

    Added solution - LeetHub

--- Reverse a String - GFG/reverse-a-string.cpp
+++ Reverse a String - GFG/reverse-a-string.cpp
@@ -15,14 +15,14 @@ class Solution
     string reverseWord(string str)
     {
         // Your code goes here
-       int start = 0;
-       int end = str.size()-1;
-       while(start <= end){
-           swap(str[start],str[end]);
-           start++;
-           end--;
-       }
-       return str; 
+        int start = 0;
+        int end = str.size()-1;
+        while(start <= end){
+            swap(str[start],str[end]);
+            start++;
+            end--;
+        }
+        return str;
     }
 };
 
