62ada4f27db29f986301c2da6ac417f4caf081d4
Sanket <95070523+Sanketkatkade@users.noreply.github.com>
2024-02-05 11:15:56 +0530

    Time: 6 ms (64.53%), Space: 11.9 MB (23.01%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1089-duplicate-zeros/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1089-duplicate-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int maxSize = arr.size();
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] == 0){
+                arr.insert(arr.begin() + i , 0);
+                i++;
+                arr.pop_back();
+            }
+        }
+    }
+};
